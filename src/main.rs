2ccb8ac0620e3df7f5672865c06604b80e313586
Hazemorn <nikitmiheenko@gmail.com>
2023-04-02 21:57:01 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use image::{GenericImageView};
+
+
+fn get_image(dir: &str, scale: u32){
+    let im =image::open(dir).unwrap();
+    println!()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
